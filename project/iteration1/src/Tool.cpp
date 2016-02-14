272c41f11afe394353ca6db7d631ae9521e0b32e
Jacob Grafenstein <grafe014@umn.edu>
2016-02-14 16:33:34 -0600

    Program now compiles. Removed function for background color dependency. Eraser subclass can simply get called with the background color in its constructor.

--- project/iteration1/src/Tool.cpp
+++ project/iteration1/src/Tool.cpp
@@ -5,9 +5,7 @@
 
 #include <iostream>
 #include <cstring>
-#include "PixelBuffer.h"
 #include "Tool.h"
-#include "ColorData.h"
 
 using std::cerr;
 using std::endl;
@@ -15,8 +13,4 @@ using std::fill;
 
 bool Tool::isActive() {
   return active;
-}
-
-ColorData Tool::getBackgroundColor(PixelBuffer) {
-  return PixelBuffer::getBackgroundColor();
 }
--- project/iteration1/src/Tool.h
+++ project/iteration1/src/Tool.h
@@ -3,29 +3,21 @@
 // Created by Jacob Grafenstein
 //
 
-#ifndef TOOL_H
-#define TOOL_H
-
 class PixelBuffer;
 class ColorData;
 
-class Tool {
-
- public:
+class Tool
+{
+public:
 
   // This function will always be overwritten by the Tool subclass
-  virtual float[][] computeMask() = 0;
+  virtual float** computeMask() = 0;
 
   // Checks if the given tool is the active tool
   bool isActive();
 
-  // Gets the background color of pixelBuffer
-  ColorData getBackgroundColor(PixelBuffer);
-
-
- private:
+private:
   float **mask;
-  float opacity;
   bool active;
 
 };
