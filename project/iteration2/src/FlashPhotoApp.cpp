eb8452d37ec9bf3f465cb66e53945b3f2a0e5736
Jacob Grafenstein <grafe014@umn.edu>
2016-04-01 10:01:14 -0500

    Undo and Redo are completed and functional

--- project/iteration2/src/FlashPhotoApp.cpp
+++ project/iteration2/src/FlashPhotoApp.cpp
@@ -23,7 +23,7 @@ FlashPhotoApp::FlashPhotoApp(int argc, char* argv[], int width, int height, Colo
     initializeTools();
     // Initialize Interface
     initializeBuffers(backgroundColor, width, height);
-    
+
     initGlui();
     initGraphics();
     backColor = backgroundColor;
@@ -31,7 +31,7 @@ FlashPhotoApp::FlashPhotoApp(int argc, char* argv[], int width, int height, Colo
     canvasHeight = height;
     previousX = -1;
     previousY = -1;
-	
+
 }
 
 void FlashPhotoApp::display()
@@ -84,7 +84,7 @@ void FlashPhotoApp::mouseDragged(int x, int y)
 
 void FlashPhotoApp::mouseMoved(int x, int y)
 {
-    
+
 }
 
 void FlashPhotoApp::leftMouseDown(int x, int y)
@@ -169,7 +169,7 @@ void FlashPhotoApp::initGlui()
 {
     // Select first tool (this activates the first radio button in glui)
     m_curTool = 0;
-    
+
     GLUI_Panel *toolPanel = new GLUI_Panel(m_glui, "Tool Type");
     {
         GLUI_RadioGroup *radio = new GLUI_RadioGroup(toolPanel, &m_curTool, UI_TOOLTYPE, s_gluicallback);
@@ -183,21 +183,21 @@ void FlashPhotoApp::initGlui()
         new GLUI_RadioButton(radio, "Blur");
 //        new GLUI_RadioButton(radio, "Stamp");//will implement later
     }
-    
+
     GLUI_Panel *colorPanel = new GLUI_Panel(m_glui, "Tool Color");
     {
         m_curColorRed = 0;
         m_gluiControlHooks.spinnerRed  = new GLUI_Spinner(colorPanel, "Red:", &m_curColorRed, UI_COLOR_R, s_gluicallback);
         m_gluiControlHooks.spinnerRed->set_float_limits(0, 1.0);
-        
+
         m_curColorGreen = 0;
         m_gluiControlHooks.spinnerGreen  = new GLUI_Spinner(colorPanel, "Green:", &m_curColorGreen, UI_COLOR_G, s_gluicallback);
         m_gluiControlHooks.spinnerGreen->set_float_limits(0, 1.0);
-        
+
         m_curColorBlue = 0;
         m_gluiControlHooks.spinnerBlue  = new GLUI_Spinner(colorPanel, "Blue:", &m_curColorBlue, UI_COLOR_B, s_gluicallback);
         m_gluiControlHooks.spinnerBlue->set_float_limits(0, 1.0);
-        
+
         new GLUI_Button(colorPanel, "Red", UI_PRESET_RED, s_gluicallback);
         new GLUI_Button(colorPanel, "Orange", UI_PRESET_ORANGE, s_gluicallback);
         new GLUI_Button(colorPanel, "Yellow", UI_PRESET_YELLOW, s_gluicallback);
@@ -207,18 +207,18 @@ void FlashPhotoApp::initGlui()
         new GLUI_Button(colorPanel, "White", UI_PRESET_WHITE, s_gluicallback);
         new GLUI_Button(colorPanel, "Black", UI_PRESET_BLACK, s_gluicallback);
     }
-    
+
     // UNDO,REDO,QUIT
     {
         m_gluiControlHooks.undoButton = new GLUI_Button(m_glui, "Undo", UI_UNDO, s_gluicallback);
-        undoEnabled(false);
+        undoEnabled(true);
         m_gluiControlHooks.redoButton  = new GLUI_Button(m_glui, "Redo", UI_REDO, s_gluicallback);
-        redoEnabled(false);
-        
+        redoEnabled(true);
+
         new GLUI_Separator(m_glui);
         new GLUI_Button(m_glui, "Quit", UI_QUIT, (GLUI_Update_CB)exit);
     }
-    
+
     new GLUI_Column(m_glui, true);
     GLUI_Panel *filterPanel = new GLUI_Panel(m_glui, "Filters");
     {
@@ -226,39 +226,39 @@ void FlashPhotoApp::initGlui()
         {
             GLUI_Spinner * filterBlurAmount = new GLUI_Spinner(blurPanel, "Amount:", &m_filterParameters.blur_amount);
             filterBlurAmount->set_int_limits(0, 20);
-            
+
             filterBlurAmount->set_int_val(5);
-            
+
             new GLUI_Button(blurPanel, "Apply", UI_APPLY_BLUR, s_gluicallback);
         }
-        
+
         GLUI_Panel *motionBlurPanel = new GLUI_Panel(filterPanel, "MotionBlur");
         {
             GLUI_Spinner * filterMotionBlurAmount = new GLUI_Spinner(motionBlurPanel, "Amount:", &m_filterParameters.motionBlur_amount);
             filterMotionBlurAmount->set_int_limits(0, 100);
-            
+
             filterMotionBlurAmount->set_int_val(5);
-            
+
             m_filterParameters.motionBlur_direction = 0;
             GLUI_RadioGroup *dirBlur = new GLUI_RadioGroup(motionBlurPanel, &m_filterParameters.motionBlur_direction);
             new GLUI_RadioButton(dirBlur, "North/South");
             new GLUI_RadioButton(dirBlur, "East/West");
             new GLUI_RadioButton(dirBlur, "NorthEast/SouthWest");
             new GLUI_RadioButton(dirBlur, "NorthWest/SouthEast");
-            
+
             new GLUI_Button(motionBlurPanel, "Apply", UI_APPLY_MOTION_BLUR, s_gluicallback);
         }
         GLUI_Panel *sharpenPanel = new GLUI_Panel(filterPanel, "Sharpen");
         {
             GLUI_Spinner * filterSharpAmount = new GLUI_Spinner(sharpenPanel, "Amount:", &m_filterParameters.sharpen_amount);
             filterSharpAmount->set_int_limits(0, 100);
-            
+
             filterSharpAmount->set_int_val(5);
-            
+
             new GLUI_Button(sharpenPanel, "Apply", UI_APPLY_SHARP, s_gluicallback);
         }
         GLUI_Panel *edgeDetPanel = new GLUI_Panel(filterPanel, "Edge Detect");
-        
+
         {
             new GLUI_Button(edgeDetPanel, "Apply", UI_APPLY_EDGE, s_gluicallback);
         }
@@ -267,76 +267,76 @@ void FlashPhotoApp::initGlui()
             GLUI_Spinner * filterThresholdAmount = new GLUI_Spinner(thresPanel, "Level:", &m_filterParameters.threshold_amount);
             filterThresholdAmount->set_float_limits(0, 1);
             filterThresholdAmount->set_float_val(0.5);
-            
+
             new GLUI_Button(thresPanel, "Apply", UI_APPLY_THRESHOLD, s_gluicallback);
         }
 
 		new GLUI_Column(filterPanel, true);
-        
+
         GLUI_Panel *saturPanel = new GLUI_Panel(filterPanel, "Saturation");
         {
             GLUI_Spinner * filterSaturationAmount = new GLUI_Spinner(saturPanel, "Amount:", &m_filterParameters.saturation_amount);
             filterSaturationAmount->set_float_limits(-10, 10);
             filterSaturationAmount->set_float_val(1);
-            
+
             new GLUI_Button(saturPanel, "Apply", UI_APPLY_SATURATE, s_gluicallback);
         }
-        
+
         GLUI_Panel *channelPanel = new GLUI_Panel(filterPanel, "Channels");
         {
             GLUI_Spinner * filterChannelRed = new GLUI_Spinner(channelPanel, "Red:", &m_filterParameters.channel_colorRed);
             GLUI_Spinner * filterChannelGreen = new GLUI_Spinner(channelPanel, "Green:", &m_filterParameters.channel_colorGreen);
             GLUI_Spinner * filterChannelBlue = new GLUI_Spinner(channelPanel, "Blue:", &m_filterParameters.channel_colorBlue);
-            
+
             filterChannelRed->set_float_limits(0, 10);
             filterChannelRed->set_float_val(1);
             filterChannelGreen->set_float_limits(0, 10);
             filterChannelGreen->set_float_val(1);
             filterChannelBlue->set_float_limits(0, 10);
             filterChannelBlue->set_float_val(1);
-            
+
             new GLUI_Button(channelPanel, "Apply", UI_APPLY_CHANNEL, s_gluicallback);
         }
-        
+
         GLUI_Panel *quantPanel = new GLUI_Panel(filterPanel, "Quantize");
         {
             GLUI_Spinner * filterQuantizeBins = new GLUI_Spinner(quantPanel, "Bins:", &m_filterParameters.quantize_bins);
             filterQuantizeBins->set_int_limits(2, 256);
             filterQuantizeBins->set_int_val(8);
             filterQuantizeBins->set_speed(0.1);
-            
+
             new GLUI_Button(quantPanel, "Apply", UI_APPLY_QUANTIZE, s_gluicallback);
         }
-        
+
         GLUI_Panel *specialFilterPanel = new GLUI_Panel(filterPanel, "Special Filter"); // YOUR SPECIAL FILTER PANEL
         {
             new GLUI_Button(specialFilterPanel, "Apply", UI_APPLY_SPECIAL_FILTER, s_gluicallback);
         }
     }
-    
+
     new GLUI_Column(m_glui, true);
 
     GLUI_Panel *imagePanel = new GLUI_Panel(m_glui, "Image I/O");
     {
         m_gluiControlHooks.fileBrowser = new GLUI_FileBrowser(imagePanel, "Choose Image", false, UI_FILE_BROWSER, s_gluicallback);
-        
+
         m_gluiControlHooks.fileBrowser->set_h(400);
-        
+
         m_gluiControlHooks.fileNameBox = new     GLUI_EditText( imagePanel , "Image:", m_fileName, UI_FILE_NAME, s_gluicallback );
         m_gluiControlHooks.fileNameBox->set_w(200);
 
         new GLUI_Separator(imagePanel);
-        
+
         m_gluiControlHooks.currentFileLabel = new GLUI_StaticText(imagePanel, "Will load image: none");
         m_gluiControlHooks.loadCanvasButton = new GLUI_Button(imagePanel, "Load Canvas", UI_LOAD_CANVAS_BUTTON, s_gluicallback);
         m_gluiControlHooks.loadStampButton = new GLUI_Button(imagePanel, "Load Stamp", UI_LOAD_STAMP_BUTTON, s_gluicallback);
-        
+
         new GLUI_Separator(imagePanel);
 
         m_gluiControlHooks.saveFileLabel = new GLUI_StaticText(imagePanel, "Will save image: none");
-        
+
         m_gluiControlHooks.saveCanvasButton = new GLUI_Button(imagePanel, "Save Canvas", UI_SAVE_CANVAS_BUTTON, s_gluicallback);
-        
+
         loadCanvasEnabled(false);
         loadStampEnabled(false);
         saveCanvasEnabled(false);
@@ -359,7 +359,7 @@ void FlashPhotoApp::initGraphics() {
 
 void FlashPhotoApp::gluiControl(int controlID)
 {
-    
+
     switch (controlID) {
         case UI_PRESET_RED:
             m_curColorRed = 1;
@@ -465,7 +465,7 @@ void FlashPhotoApp::gluiControl(int controlID)
         default:
             break;
     }
-    
+
     // Forces canvas to update changes made in this function
     m_glui->post_update_main_gfx();
 }
@@ -547,23 +547,38 @@ void FlashPhotoApp::applyFilterSpecial() {
 
 void FlashPhotoApp::undoOperation()
 {
+  if (!undoStack.empty()) {
     cout << "Undoing..." << endl;
-	// Pull tempPixelBuffer off the undostack
-
-	// Put m_displayBuffer on redoStack
-
-	// Set m_displayBuffer to tempPixelBuffer
+  	// Pull tempPixelBuffer off the undostack
+    PixelBuffer *myNewPixelBuffer = undoStack.back();
+    undoStack.pop_back();
+  	// Put m_displayBuffer on redoStack
+    PixelBuffer *tempPixelBuffer = new PixelBuffer(canvasWidth,canvasHeight,backColor);
+  	m_displayBuffer->copyPixelBuffer(m_displayBuffer, tempPixelBuffer);
+  	redoStack.push_back(tempPixelBuffer);
+  	// Set m_displayBuffer to tempPixelBuffer
+    m_displayBuffer = myNewPixelBuffer;
+  } else {
+    cout << "Nothing to undo" << endl;
+  }
 }
 
 void FlashPhotoApp::redoOperation()
 {
+  if (!redoStack.empty()) {
     cout << "Redoing..." << endl;
-	// Pull displayBuffer off of redoStack
-
-	// Put m_displayBuffer on undoStack
-
-	// Set m_displayBuffer to displayBuffer
-
+  	// Pull displayBuffer off of redoStack
+    PixelBuffer *myNewPixelBuffer = redoStack.back();
+    redoStack.pop_back();
+  	// Put m_displayBuffer on undoStack
+    PixelBuffer *tempPixelBuffer = new PixelBuffer(canvasWidth,canvasHeight,backColor);
+  	m_displayBuffer->copyPixelBuffer(m_displayBuffer, tempPixelBuffer);
+  	undoStack.push_back(tempPixelBuffer);
+  	// Set m_displayBuffer to displayBuffer
+    m_displayBuffer = myNewPixelBuffer;
+  } else {
+    cout << "Nothing to redo" << endl;
+  }
 }
 // ** END OF CALLBACKS **
 // **********************
@@ -616,7 +631,7 @@ bool FlashPhotoApp::hasSuffix(const std::string & str, const std::string & suffi
 
 
 bool FlashPhotoApp::isValidImageFileName(const std::string & name) {
-    
+
     if (hasSuffix(name, ".png")
         || hasSuffix(name, ".jpg")
         || hasSuffix(name, ".jpeg")
@@ -627,7 +642,7 @@ bool FlashPhotoApp::isValidImageFileName(const std::string & name) {
 }
 
 bool FlashPhotoApp::isValidImageFile(const std::string & name) {
-    
+
     FILE *f;
     bool isValid = false;
     if (isValidImageFileName(name)) {
@@ -648,14 +663,14 @@ void FlashPhotoApp::setImageFile(const std::string & fileName)
     if (!isValidImageFileName(imageFile)) {
         imageFile = m_fileName;
     }
-    
-    
+
+
     // TOGGLE SAVE FEATURE
     // If no file is selected or typed,
     // don't allow file to be saved. If
     // there is a file name, then allow
     // file to be saved to that name.
-    
+
     if (!isValidImageFileName(imageFile)) {
         m_gluiControlHooks.saveFileLabel->set_text("Will save image: none");
         saveCanvasEnabled(false);
@@ -663,15 +678,15 @@ void FlashPhotoApp::setImageFile(const std::string & fileName)
         m_gluiControlHooks.saveFileLabel->set_text((std::string("Will save image: ") + imageFile).c_str());
         saveCanvasEnabled(true);
     }
-    
+
     // TOGGLE LOAD FEATURE
-    
+
     // If the file specified cannot be opened,
     // then disable stamp and canvas loading.
     if (isValidImageFile(imageFile)) {
         loadStampEnabled(true);
         loadCanvasEnabled(true);
-        
+
         m_gluiControlHooks.currentFileLabel->set_text((std::string("Will load: ") + imageFile).c_str());
         m_gluiControlHooks.fileNameBox->set_text(imageFile);
     } else {
