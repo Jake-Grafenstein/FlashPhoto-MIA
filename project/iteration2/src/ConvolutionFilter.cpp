5cd687b12936d47d65375d1b42a03b31076d71d5
Jonathon <meye1907@umn.edu>
2016-04-08 12:57:18 -0500

    Lots of jpeg files redone, guards on ConvolutionFilter, jpeg path fixed in Makefile

--- project/iteration2/src/ConvolutionFilter.cpp
+++ project/iteration2/src/ConvolutionFilter.cpp
@@ -32,16 +32,23 @@ void ConvolutionFilter::applyFilter(PixelBuffer *buf, float amount, int directio
 void ConvolutionFilter::applyKernel(int x, int y,PixelBuffer *buf, PixelBuffer *temp)
 {
 	float r=0,g=0,b=0;
-	int i,j;
+	int i,j,xloc,yloc,w,h;
 	ColorData pixel;
+	w = buf->getWidth();
+	h = buf->getHeight();
 	for (i = 0; i < kernelSize; i++)
 	{
 		for (j = 0; j < kernelSize; j++)
 		{
-			pixel = buf -> getPixel(x + i - (kernelSize/2),y + j - (kernelSize/2));
-			r += (kernel[i][j] * pixel.getRed());
-			g += (kernel[i][j] * pixel.getGreen());
-			b += (kernel[i][j] * pixel.getBlue());
+			xloc = x+i-(kernelSize/2);
+			yloc = y+j-(kernelSize/2);
+			if (xloc > -1 && xloc < w && yloc > -1 && yloc < h)
+			{
+				pixel = buf -> getPixel(x + i - (kernelSize/2),y + j - (kernelSize/2));
+				r += (kernel[i][j] * pixel.getRed());
+				g += (kernel[i][j] * pixel.getGreen());
+				b += (kernel[i][j] * pixel.getBlue());
+			}
 		}
 	}
 	temp -> setPixel(x,y,ColorData(r,g,b));
