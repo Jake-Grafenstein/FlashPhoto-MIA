5669eae98cf7a0540626cf096bcc52b10d934ba5
Jonathon <meye1907@umn.edu>
2016-05-04 12:35:21 -0500

    Added tests for the tools in FlashPhoto

--- project/iteration3/tests/test_eraser.cpp
+++ project/iteration3/tests/test_eraser.cpp
@@ -3,18 +3,18 @@
 int main(int argc,char *argv[])
 {
 	FlashPhotoApp *prog = new FlashPhotoApp(argc, argv, 800,800,ColorData(1,1,0.95));
-	prog->setTool(0);
+	prog->setTool(1);
 	prog->mouseDragged(200,100);
 	prog->setFileName("Gold standard file here");
 	prog->gluiControl(UI_FILE_NAME);
 	prog->gluiControl(UI_LOAD_STAMP_BUTTON);//saves canvas
 	if (prog->compareBuffers())
 	{
-		printf("Testing: Pen\t SUCCESS\n");
+		printf("Testing: Eraser\t SUCCESS\n");
 	}
 	else
 	{
-		printf("Testing: Pen\n FAIL\n");
+		printf("Testing: Eraser\t FAIL\n");
 	}
 	return 0;
 }
--- project/iteration3/tests/test_stamp.cpp
+++ project/iteration3/tests/test_stamp.cpp
@@ -3,18 +3,21 @@
 int main(int argc,char *argv[])
 {
 	FlashPhotoApp *prog = new FlashPhotoApp(argc, argv, 800,800,ColorData(1,1,0.95));
-	prog->setTool(0);
-	prog->mouseDragged(200,100);
+	prog->setTool(7);
+	prog->setFileName("Some file to stamp here");
+	prog->gluiControl(UI_FILE_NAME);
+	prog->gluiControl(UI_LOAD_STAMP_BUTTON);
+	prog->mouseDown(200,100);
 	prog->setFileName("Gold standard file here");
 	prog->gluiControl(UI_FILE_NAME);
 	prog->gluiControl(UI_LOAD_STAMP_BUTTON);//saves canvas
 	if (prog->compareBuffers())
 	{
-		printf("Testing: Pen\t SUCCESS\n");
+		printf("Testing: Stamp\t SUCCESS\n");
 	}
 	else
 	{
-		printf("Testing: Pen\n FAIL\n");
+		printf("Testing: Stamp\t FAIL\n");
 	}
 	return 0;
 }
