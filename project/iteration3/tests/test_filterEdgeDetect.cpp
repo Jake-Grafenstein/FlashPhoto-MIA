577a283a608e6a4942b5d7fccdb1088672d4e5c6
Live session user <frisb043@umn.edu>
2016-05-04 15:17:16 -0500

    added filter tests

---
+++ project/iteration3/tests/test_filterEdgeDetect.cpp
@@ -0,0 +1,20 @@
+#include "../FlashPhoto/FlashPhoto.h"
+
+int main(int argc,char *argv[])
+{
+	FlashPhotoApp *prog = new FlashPhotoApp(argc, argv, 800,800,ColorData(1,1,0.95));
+	prog->loadImageTest("snaily.jpg");
+  prog->applyFilterEdgeDetect();
+  prog->setFileName("Gold standard file here");
+	prog->gluiControl(UI_FILE_NAME);
+	prog->gluiControl(UI_LOAD_STAMP_BUTTON);//saves canvas
+  if (prog->compareBuffers())
+	{
+		printf("Testing: Blur\t SUCCESS\n");
+	}
+	else
+	{
+		printf("Testing: Blur\t FAIL\n");
+	}
+  return 0;
+}
